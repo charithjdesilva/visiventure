cf8a6cb8864d615ead7c69d2c5ee9e4833cbd126
charithjdesilva <charithjdesilva@gmail.com>
2023-04-15 01:02:23 +0530

    initialized and connected VideoCard, ChannelCard

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Stack, Box } from '@mui/material';
+import { VideoCard, ChannelCard } from './';
 
 const Videos = ({ videos }) => {
   return (
-    <div>Videos</div>
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((item, index) => (
+        <Box key={index}>
+          {/* check if it is a profile or just a video */}
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
   )
 }
 
